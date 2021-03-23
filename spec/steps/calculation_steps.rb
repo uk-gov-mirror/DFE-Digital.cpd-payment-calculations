d48532bb192d40f9166047b262868ac674d5f744
Tim Abell <tim@timwise.co.uk>
2021-03-23 12:17:47 +0000

    Move decimal_placeholder to turnip_helper
    For future reuse

--- spec/steps/calculation_steps.rb
+++ spec/steps/calculation_steps.rb
@@ -7,8 +7,3 @@ end
 step 'The output should be :decimal_placeholder' do |sum|
   expect(calculator_result).to eq(sum)
 end
-
-# Convert decimal strings to decimal values. Ref: https://github.com/jnicklas/turnip#custom-step-placeholders
-placeholder :decimal_placeholder do
-  match(/\d+\.?\d+/, &:to_d)
-end
--- spec/turnip_helper.rb
+++ spec/turnip_helper.rb
@@ -1,3 +1,8 @@
 # This file is automatically loaded by the BDD gem turnip https://github.com/jnicklas/turnip#where-to-place-steps
 
 Dir.glob("spec/steps/**/*steps.rb") { |f| load f, true }
+
+# Convert decimal strings to decimal values. Ref: https://github.com/jnicklas/turnip#custom-step-placeholders
+placeholder :decimal_placeholder do
+  match(/\d+\.?\d+/, &:to_d)
+end
