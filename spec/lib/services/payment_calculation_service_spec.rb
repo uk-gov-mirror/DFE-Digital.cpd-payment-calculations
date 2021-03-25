5143327724e0e72233363a4c9a82127049601206
Tim Abell <tim@timwise.co.uk>
2021-03-25 17:30:07 +0000

    Calculate ECF Service Fees
    Co-authored-by: Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>

--- spec/lib/services/payment_calculation_service_spec.rb
+++ spec/lib/services/payment_calculation_service_spec.rb
@@ -1,6 +1,9 @@
 describe PaymentCalculationService do
-  it 'Returns a BigDecimal' do
-    calculator_result = PaymentCalculationService.calculate
-    expect(calculator_result).to be_a(BigDecimal)
+  it 'Return BigDecimal for all money outputs' do
+    config = { :recruitment_target => 2000, :band_a => 995, :band_b => 979, :band_c => 966, :setup_cost => 123.456 }
+    calculator = PaymentCalculationService.new(config)
+    expect(calculator.per_participant_service_fee).to be_a(BigDecimal)
+    expect(calculator.total_service_fee).to be_a(BigDecimal)
+    expect(calculator.monthly_service_fee).to be_a(BigDecimal)
   end
 end
